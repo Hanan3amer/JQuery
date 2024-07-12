0796ce13744c5ce25a601ba3ee4f5d0cd8f1843a
Hanan3amer <hananamer312003@gmail.com>
2024-07-12 14:02:09 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ closebtn = document.getElementById("close")
 closebtn.addEventListener('click', function () {
     $('aside').animate({ left: `-${width}px` }, 500)
 })
-
+$(document).ready(function () {
+    $('aside').animate({ left: `-${width}px` }, 500)
+})
 let width = $('.navs').outerWidth()
 $('.open').click(function () {
     if ($('aside').css('left') === '0px')
-        // $('aside').animate({left:`-${width}px`},500)
         closebtn.addEventListener('click', function () {
             $('aside').animate({ left: `-${width}px` }, 500)
         })
@@ -14,22 +15,28 @@ $('.open').click(function () {
         $('aside').animate({ left: `0px` }, 500)
 })
 
-$(".singer1").click(function () {
-    $(".text1").slideToggle(300)
-})
-$(".singer2").click(function () {
-    document.querySelector(".text2").classList.remove("d-none")
-    // document.querySelector(".text2").classList.add("d-block")
-    $(".text2").slideToggle(300)
-})
-$(".singer3").click(function () {
-    document.querySelector(".text3").classList.remove("d-none")
-    $(".text3").slideToggle(300)
-})
-$(".singer4").click(function () {
-    document.querySelector(".text4").classList.remove("d-none")
-    $(".text4").slideToggle(300)
-})
+$(".text2").hide()
+$(".text3").hide()
+$(".text4").hide()
+
+$(document).ready(function () {
+    $(".singer1").click(function () {
+        $(".text1").slideToggle(300);
+    });
+
+    $(".singer2").click(function () {
+        $(".text2").slideToggle(300);
+
+    });
+
+    $(".singer3").click(function () {
+        $(".text3").slideToggle(300);
+    });
+
+    $(".singer4").click(function () {
+        $(".text4").slideToggle(300);
+    });
+});
 
 let countdown = new Date("Jul 31, 2024 23:59:59").getTime()
 
@@ -59,3 +66,9 @@ document.querySelector("textarea").addEventListener('keyup', function () {
         remain.innerHTML = count
     }
 })
+// smooth scrolling
+// $("a").click(function(e){
+//     let section = $(e.target).attr('href')
+//     let sectop = $(section).offset().top
+//     $('body,html').animate({screenTop:sectop},1000)
+// })
